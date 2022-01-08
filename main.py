09d392ae95e3741070f961ca9c6a6ae81776cce8
Story <pathakharshit281@gmail.com>
2022-01-08 10:16:55 +0530

    main.py

--- main.py
+++ main.py
@@ -3,36 +3,47 @@ import random as ran
 from sqlite3.dbapi2 import Connection, Cursor
 from typing import Mapping
 import sqlite3
+import json
 r=1
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 connection = sqlite3.connect('data.db')
 crsr = connection.cursor()
-sql_command = """CREATE TABLE data (
-    Vehicle_Number INTEGER,
-    Code INTEGER PRIMARY KEY)"""
-crsr.execute(sql_command)
+#sql_command = """CREATE TABLE data (
+#    Vehicle_Number INTEGER,
+#    Code INTEGER PRIMARY KEY)"""
+#crsr.execute(sql_command)
 
-
+def convert(list):
+      
+    # Converting integer list to string list
+    s = [str(i) for i in list]
+      
+    # Join list items using join()
+    res = int("".join(s))
+      
+    return(res)
 
 while r==1:    
     option = input("Press 1 : If you want to park your vehicle\nPress 2 : If you want to recieve your vehicle\nResponce :")
     if option == "1":
-        vehicle = input("Please enter your vehicle no. :")
+        vehicle = int(input("Please enter your vehicle no. :"))
         list = [i for i in range(100,1000)]
         a = ran.choices(list, k=1)
-        list1 = (vehicle, a)
+        b = convert(a)
+        list1 = [vehicle, b]
+        var_string = ', '.join('?' * len(list1))
+        sql = 'INSERT INTO data(Vehicle_Number, code) VALUES (%s);' % var_string
         #checking random no. is not same
-        print("your code is :",a)
-        sql_cmd = """INSERT INTO data VALUES {list1}"""
-        crsr.execute(sql_cmd)
+        print("your code is :",b)
+        crsr.execute(sql, list1)
         connection.commit()
     
     elif option == "2":
         veh = int(input("please enter your vehicle no. :"))
         
-        for i in (0,2):
+        for i in (0,3):
             code = int(input("please enter your alloted code :"))
             #checking the info in database
             crsr.execute("SELECT code FROM data WHERE (Vehicle_number = '{veh}')")
@@ -48,7 +59,7 @@ while r==1:
         
     else:
         print("Option out of bound")
-    x=input("Press y to exit :")
-    if x=="y":
-        cls()
+    x=input("Press enter to exit :")
+    cls()
+
 
